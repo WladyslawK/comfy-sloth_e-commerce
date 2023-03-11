34508b2b02ea77cdcd63c6204eb26acdd03662fa
WladyslawK <vladkurr7890@gmail.com>
2023-03-12 00:29:44 +0100

    feature: (singleProduct.js) singleProduct redirect

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,32 +1,52 @@
-import React, { useEffect } from 'react'
-import { useParams, useHistory } from 'react-router-dom'
-import { useProductsContext } from '../context/products_context'
-import { single_product_url as url } from '../utils/constants'
-import { formatPrice } from '../utils/helpers'
+import React, {useEffect} from 'react'
+import {useParams, useHistory} from 'react-router-dom'
+import {useProductsContext} from '../context/products_context'
+import {single_product_url as url} from '../utils/constants'
+import {formatPrice} from '../utils/helpers'
 import {
-  Loading,
-  Error,
-  ProductImages,
-  AddToCart,
-  Stars,
-  PageHero,
+    Loading,
+    Error,
+    ProductImages,
+    AddToCart,
+    Stars,
+    PageHero,
 } from '../components'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const SingleProductPage = () => {
 
-  const {fetchSingleProduct, single_product} = useProductsContext()
-  const routeParams = useParams()
+    const {fetchSingleProduct,
+        single_product: product,
+        single_product_loading: loading,
+        single_loading_product_error: error
+    } = useProductsContext()
+    const routeParams = useParams()
+    const history = useHistory()
 
-  useEffect(() => {
-    fetchSingleProduct(`${url}${routeParams.id}`)
-  }, [])
+    useEffect(() => {
+        fetchSingleProduct(`${url}${routeParams.id}`)
+    }, [error])
 
-  console.log(single_product)
+    useEffect(() => {
+        if(error){
+            setTimeout(() => {
+                history.push('/')
+            }, 3000)
+        }
+    }, [])
+
+    console.log(product)
+
+    if(error){
+        return <Error/>
+    }
+    if(loading){
+        return <Loading/>
+    }
 
 
-  return <h4>single product page</h4>
+    return <h4>single product page</h4>
 }
 
 const Wrapper = styled.main`
@@ -35,18 +55,22 @@ const Wrapper = styled.main`
     gap: 4rem;
     margin-top: 2rem;
   }
+
   .price {
     color: var(--clr-primary-5);
   }
+
   .desc {
     line-height: 2;
     max-width: 45em;
   }
+
   .info {
     text-transform: capitalize;
     width: 300px;
     display: grid;
     grid-template-columns: 125px 1fr;
+
     span {
       font-weight: 700;
     }
@@ -57,6 +81,7 @@ const Wrapper = styled.main`
       grid-template-columns: 1fr 1fr;
       align-items: center;
     }
+
     .price {
       font-size: 1.25rem;
     }
